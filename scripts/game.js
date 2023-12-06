5c6361043781881ced481311e075dbb0163db93f
danielanthonycollins <>
2023-12-06 19:02:08 +0000

    feat: Add a progress bar

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,8 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
-const questionCounterText = document.getElementById("questionCounter");
+const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
-
+const progressBarFull = document.getElementById("progressBarFull");
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
@@ -37,7 +37,7 @@ let questions = [
     }
 ];
 
-//CONSTANTS
+//Constants
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
 
@@ -54,7 +54,9 @@ getNewQuestion = () => {
         return window.location.assign("/end.html");
     }
     questionCounter++;
-    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+    //Update the progress bar
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
     const questionIndex = Math.floor(Math.random() * availableQuesions.length);
     currentQuestion = availableQuesions[questionIndex];
