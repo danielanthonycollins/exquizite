e886a97c27e39943e71b041e6a397166a532fcf7
danielanthonycollins <>
2023-12-20 21:09:38 +0000

    feat: Add js code to show no high scores message if there are none

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,8 +1,12 @@
 const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-highScoresList.innerHTML = highScores
-    .map(score => {
-        return `<li class="high-score">${score.name} - ${score.score}</li>`;
-    })
-    .join("");
+if (highScores.length === 0) {
+    highScoresList.innerHTML = '<p>No High Scores</p>'
+} else {
+    highScoresList.innerHTML = highScores
+        .map(score => {
+            return `<li class="high-score">${score.name} - ${score.score}</li>`;
+        })
+        .join("");
+}
