91aadaff59415d79cbee9b6988a48fda5f36f142
danielanthonycollins <>
2023-12-21 17:00:02 +0000

    Add semi colon to end of line 9 as per js hint check

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -6,7 +6,7 @@ const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 */
 
 if (highScores.length === 0) {
-    highScoresList.innerHTML = '<p>No High Scores</p>'
+    highScoresList.innerHTML = '<p>No High Scores</p>';
 } else {
     highScoresList.innerHTML = highScores
         .map(score => {
